1ac5da07d2a5556d8e5be63c6c52ee62d780ed69
Daniel Tang <tangd@google.com>
2020-02-11 15:17:29 -0800

    add an auto setter for gcloud.project.projectNumber
    This allows kpt to automatically set a project number if the relevant
    setter appears in the config, saving users from having to go look it
    up themselves. A future change should automatically set this whenever
    gcloud.core.project is set.

--- internal/util/setters/setters.go
+++ internal/util/setters/setters.go
@@ -49,6 +49,7 @@ func PerformSetters(path string) error {
 
 	// auto-fill setters from gcloud
 	gcloudConfig := []string{"compute.region", "compute.zone", "core.project"}
+	projectID := ""
 	for _, c := range gcloudConfig {
 		gcloudCmd := exec.Command("gcloud",
 			"config", "list", "--format", fmt.Sprintf("value(%s)", c))
@@ -62,7 +63,17 @@ func PerformSetters(path string) error {
 			// don't replace values that aren't set - stick with the defaults as defined in the manifest
 			continue
 		}
+		if c == "core.project" {
+			projectID = v
+		}
 		fltrs = append(fltrs, &setters.PerformSetters{Name: fmt.Sprintf("gcloud.%s", c), Value: v, SetBy: "kpt"})
+	}
+
+	if projectID != "" {
+		projectNumber, err := GetProjectNumberFromProjectID(projectID)
+		if err == nil && projectNumber != "" {
+			fltrs = append(fltrs, &setters.PerformSetters{Name: "gcloud.project.projectNumber", Value: projectNumber, SetBy: "kpt"})
+		}
 	}
 
 	if len(fltrs) == 0 {
@@ -71,4 +82,16 @@ func PerformSetters(path string) error {
 
 	return kio.Pipeline{Inputs: []kio.Reader{rw}, Filters: fltrs, Outputs: []kio.Writer{rw}}.
 		Execute()
+}
+
+func GetProjectNumberFromProjectID(projectID string) (string, error) {
+	gcloudCmd := exec.Command("gcloud",
+		"projects", "describe", projectID, "--format", "value(projectNumber)")
+	b, err := gcloudCmd.Output()
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return "", err
+	}
+	fmt.Println(strings.TrimSpace(string(b)))
+	return strings.TrimSpace(string(b)), nil
 }
