dca5b8dc4c4d67f7645e20dd95e2e2c2386b5a6a
Phani Teja Marupaka <pmarupaka@google.com>
2021-02-12 09:32:22 -0800

    Remove sync command from next

--- commands/commands.go
+++ commands/commands.go
@@ -22,7 +22,6 @@ import (
 	"github.com/GoogleContainerTools/kpt/internal/cmdfix"
 	"github.com/GoogleContainerTools/kpt/internal/cmdget"
 	"github.com/GoogleContainerTools/kpt/internal/cmdinit"
-	"github.com/GoogleContainerTools/kpt/internal/cmdsync"
 	"github.com/GoogleContainerTools/kpt/internal/cmdupdate"
 	"github.com/spf13/cobra"
 	"k8s.io/kubectl/pkg/cmd/util"
@@ -31,7 +30,6 @@ import (
 func GetAnthosCommands(name string) []*cobra.Command {
 	c := []*cobra.Command{cmddesc.NewCommand(name),
 		cmdget.NewCommand(name), cmdinit.NewCommand(name),
-		cmdsync.NewCommand(name),
 		cmdupdate.NewCommand(name), cmddiff.NewCommand(name),
 		cmdfix.NewCommand(name),
 	}
--- demos/04-sync-get-update/README.md
+++
@@ -1,7 +0,0 @@
-# Get Run
-
-This demo:
- 
-- uses sync to fetch the [helloworld-set](../../package-examples/helloworld-set) package at
-  2 different versions into separate directories
-- uses sync to update the version of a package
