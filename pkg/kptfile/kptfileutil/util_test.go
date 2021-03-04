73e87aa093e3e937a407cbf7636c41b48f6b1aef
Morten Torkildsen <mortent@google.com>
2021-03-04 10:04:11 -0800

    [WIP] Add some tests for demonstrating how a Kptfile pipeline will be merged

--- pkg/kptfile/kptfileutil/util_test.go
+++ pkg/kptfile/kptfileutil/util_test.go
@@ -24,6 +24,7 @@ import (
 	kptfilev1alpha2 "github.com/GoogleContainerTools/kpt/pkg/api/kptfile/v1alpha2"
 	"github.com/stretchr/testify/assert"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
+	"sigs.k8s.io/kustomize/kyaml/yaml/merge3"
 )
 
 // TestValidateInventory tests the ValidateInventory function.
@@ -590,3 +591,364 @@ func TestKptFile_MergeSubpackages(t *testing.T) {
 		})
 	}
 }
+
+func TestMerge(t *testing.T) {
+	testCases := map[string]struct{
+		origin      string
+		update      string
+		local       string
+		expected    string
+		err error
+	} {
+		// With no associative key, there is no merge, just a replacement
+		// of the pipeline with upstream. This is aligned with the general behavior
+		// of kyaml merge where in conflicts the upstream version win.s
+		"no associative key, additions in both upstream and local": {
+			origin: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+`,
+			update: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - image: gcr.io/kpt/gen-folders
+`,
+			local: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - image: gcr.io/kpt/folder-ref
+`,
+			expected: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - image: gcr.io/kpt/gen-folders
+`,
+		},
+
+		// When adding an associative key, we get a real merge of the pipeline.
+		// In this case, we have an initial empty list in origin and different
+		// functions are added in upstream and local. In this case the element
+		// added in local are placed first in the resulting list.
+		"associative key name, additions in both upstream and local": {
+			origin: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+`,
+			update: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: gen-folders
+    image: gcr.io/kpt/gen-folders
+`,
+			local: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: folder-ref
+    image: gcr.io/kpt/folder-ref
+`,
+			// The reordering of elements in the results is a bug in the
+			// merge logic I think.
+			expected: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: folder-ref
+    image: gcr.io/kpt/folder-ref
+  - image: gcr.io/kpt/gen-folders
+    name: gen-folders 
+`,
+		},
+
+
+		// Even with multiple elements added in both upstream and local, all
+		// elements from local comes before upstream, and the order of elements
+		// from each source is preserved. There is no lexicographical
+		// ordering.
+		"associative key name, multiple additions in both upstream and local": {
+			origin: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+`,
+			update: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: z-upstream
+    image: z-gcr.io/kpt/gen-folders
+  - name: a-upstream
+    image: a-gcr.io/kpt/gen-folders
+`,
+			local: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: x-local
+    image: x-gcr.io/kpt/gen-folders
+  - name: b-local
+    image: b-gcr.io/kpt/gen-folders
+`,
+			expected: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: x-local
+    image: x-gcr.io/kpt/gen-folders
+  - name: b-local
+    image: b-gcr.io/kpt/gen-folders
+  - image: z-gcr.io/kpt/gen-folders
+    name: z-upstream
+  - image: a-gcr.io/kpt/gen-folders
+    name: a-upstream
+`,
+		},
+
+
+		// If elements with the same associative key are added in both upstream
+		// and local, it will be merged. It will keep the location in the list
+		// from local.
+		"same element in both local and upstream does not create duplicate": {
+			origin: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+`,
+			update: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: gen-folder-upstream
+    image: gcr.io/kpt/gen-folders
+  - name: ref-folders
+    image: gcr.io/kpt/ref-folders
+    configMap:
+      foo: bar
+`,
+			local: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: ref-folders
+    image: gcr.io/kpt/ref-folders
+    configMap:
+      bar: foo
+  - name: gen-folder-local
+    image: gcr.io/kpt/gen-folders
+`,
+			expected: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: ref-folders
+    image: gcr.io/kpt/ref-folders
+    configMap:
+      bar: foo
+      foo: bar
+  - name: gen-folder-local
+    image: gcr.io/kpt/gen-folders
+  - image: gcr.io/kpt/gen-folders
+    name: gen-folder-upstream
+`,
+		},
+
+
+		// If a field are set in both upstream and local, the value from
+		// upstream will be chosen.
+		"If there is a field-level conflict, upstream will win": {
+			origin: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+`,
+			update: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: ref-folders
+    image: gcr.io/kpt/ref-folders
+    configMap:
+      band: sleater-kinney
+`,
+			local: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: ref-folders
+    image: gcr.io/kpt/ref-folders
+    configMap:
+      band: Hüsker Dü
+`,
+			expected: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: ref-folders
+    image: gcr.io/kpt/ref-folders
+    configMap:
+      band: sleater-kinney
+`,
+		},
+
+
+		// KRM resources as parameters to functions are also merged.
+		"KRM resources in the pipeline are merged": {
+			origin: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+`,
+			update: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: myCRD-gen
+    image: gcr.io/kpt/crd-gen
+    config:
+      apiVersion: kpt.dev/v1alpha2
+      kind: MyCRD
+      metadata:
+        name: foo
+        namespace: default
+        labels:
+          upstream: def456
+      spec:
+        replicas: 4
+        list:
+        - a
+        - b
+`,
+			local: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: myCRD-gen
+    image: gcr.io/kpt/crd-gen
+    config:
+      apiVersion: kpt.dev/v1alpha2
+      kind: MyCRD
+      metadata:
+        name: doo
+        namespace: default
+        labels:
+          local: abc123
+      spec:
+        replicas: 42
+        list:
+        - a
+        - c
+`,
+			expected: `
+apiVersion: kpt.dev/v1alpha2
+kind: Kptfile
+metadata:
+  name: pipeline
+pipeline:
+  mutators:
+  - name: myCRD-gen
+    image: gcr.io/kpt/crd-gen
+    config:
+      apiVersion: kpt.dev/v1alpha2
+      kind: MyCRD
+      metadata:
+        name: foo
+        namespace: default
+        labels:
+          local: abc123
+          upstream: def456
+      spec:
+        replicas: 4
+        list:
+        - a
+        - b
+`,
+		},
+	}
+	for tn, tc := range testCases {
+		t.Run(tn, func(t *testing.T) {
+			actual, err := merge3.MergeStrings(tc.local, tc.origin, tc.update, true)
+			if tc.err == nil {
+				if !assert.NoError(t, err) {
+					t.FailNow()
+				}
+				if !assert.Equal(t,
+					strings.TrimSpace(tc.expected), strings.TrimSpace(actual)) {
+					t.FailNow()
+				}
+			} else {
+				if !assert.Error(t, err) {
+					t.FailNow()
+				}
+				if !assert.Contains(t, tc.err.Error(), err.Error()) {
+					t.FailNow()
+				}
+			}
+		})
+	}
+}
+
+
+
