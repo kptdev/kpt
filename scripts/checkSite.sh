1ce042147f0ee03468c86ac394e5baa1271d14bc
Eyob Tefera <extaeger@google.com>
2021-03-09 20:37:14 +0000

    Add success message to script.

--- scripts/checkSite.sh
+++ scripts/checkSite.sh
@@ -24,3 +24,4 @@ echo "Starting check."
 find . -name README.md -printf "%P\n" | grep -v node_modules | grep -v themes \
     | sed "s/README.md//" | sed "s/^/http:\/\/localhost:3000\//" \
     | xargs -n1 sh -c '! (npx href-checker $0 --bad-content="404 - Not found" --silent --no-off-site -c=15 | grep .) || exit 255'
+echo "Success."
