0ca19a005c9f9c93638896a3d03ef93a49961a74
Eyob Tefera <extaeger@google.com>
2021-03-12 21:48:04 +0000

    Better comment site checking script.

--- scripts/checkSite.sh
+++ scripts/checkSite.sh
@@ -21,7 +21,10 @@ docker run -p 3000:80 -d kpt-site-check:latest
 cd site
 npm i
 echo "Starting check."
+# Find all potential entrypoint READMEs while stripping out themes directory until it's removed
+# Convert them to valid URLs
+# Check each link for Docsify 404s
 find . -name README.md -printf "%P\n" | grep -v node_modules | grep -v themes \
     | sed "s/README.md//" | sed "s/^/http:\/\/localhost:3000\//" \
-    | xargs -n1 sh -c '! (npx href-checker $0 --bad-content="404 - Not found" --silent --no-off-site -c=15 | grep .) || exit 255'
+    | xargs -n1 sh -c '! (npx href-checker $0 --bad-content="404 - Not found" --silent --no-off-site -c=15 | grep .) || exit 255' 
 echo "Success."
