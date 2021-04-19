13e3c43c741144c09afd185ed571d8a210a70b17
Sam Wronski <samwronski@google.com>
2021-04-19 15:54:27 -0700

    Pipelines cause diff

--- internal/util/diff/pkgdiff_test.go
+++ internal/util/diff/pkgdiff_test.go
@@ -63,6 +63,20 @@ func TestPkgDiff(t *testing.T) {
 			diff: toStringSet(),
 		},
 		{
+			name: "different pipelines in Kptfile is a diff",
+			pkg1: pkgbuilder.NewRootPkg().
+				WithKptfile(pkgbuilder.NewKptfile().
+					WithPipeline(pkgbuilder.NewFunction("gcr.io/kpt-dev/foo:latest")).
+					WithUpstream("github.com/GoogleContainerTools/kpt", "/", "master", "resource-merge")).
+				WithResource(pkgbuilder.DeploymentResource),
+			pkg2: pkgbuilder.NewRootPkg().
+				WithKptfile(pkgbuilder.NewKptfile().
+					WithPipeline(pkgbuilder.NewFunction("gcr.io/kpt-dev/bar:latest")).
+					WithUpstream("github.com/GoogleContainerTools/kpt", "/", "kpt/v1", "resource-merge")).
+				WithResource(pkgbuilder.DeploymentResource),
+			diff: toStringSet("Kptfile"),
+		},
+		{
 			name: "subpackages are not included",
 			pkg1: pkgbuilder.NewRootPkg().
 				WithKptfile().
