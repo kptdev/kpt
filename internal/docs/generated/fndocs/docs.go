d4da665750f822150afd38accf84684629b2dfeb
droot <sunilarora@google.com>
2021-05-14 10:24:20 -0700

    fixed chaining example

--- internal/docs/generated/fndocs/docs.go
+++ internal/docs/generated/fndocs/docs.go
@@ -199,7 +199,7 @@ var SinkExamples = `
   # read resources from DIR directory, execute my-fn on them and write the
   # output to DIR directory.
   $ kpt fn source DIR |
-    kpt fn eval --image gcr.io/example.com/my-fn |
+    kpt fn eval --image gcr.io/example.com/my-fn - |
     kpt fn sink DIR
 `
 
@@ -226,6 +226,6 @@ var SourceExamples = `
   # read resources from DIR directory, execute my-fn on them and write the
   # output to DIR directory.
   $ kpt fn source DIR |
-    kpt fn eval --image gcr.io/example.com/my-fn |
+    kpt fn eval --image gcr.io/example.com/my-fn - |
     kpt fn sink DIR
 `
