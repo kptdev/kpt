aa30e5fb9bc8ea5ead12ebe74452910ec8145c36
Eyob Tefera <extaeger@google.com>
2021-05-16 09:29:50 +0000

    Move configuration into separate JS file.

---
+++ site/static/js/config.js
@@ -0,0 +1,32 @@
+window.$docsify = {
+  name: `<img src="/static/images/logo.svg" alt="kpt" />`,
+  nameLink: window.location.origin,
+  search: {
+    maxAge: 43200000,
+    placeholder: "Search",
+    paths: "auto",
+  },
+  loadSidebar: "sidebar.md",
+  alias: {
+    "/*.*/sidebar.md": "/sidebar.md",
+  },
+  coverpage: "coverpage.md",
+  crossOriginLinks: ["https://catalog.kpt.dev"],
+  auto2top: true,
+  repo: "true",
+  pagination: {
+    crossChapter: true,
+  },
+  corner: {
+    url: "https://github.com/GoogleContainerTools/kpt",
+    icon: "github",
+  },
+  routerMode: "history",
+  markdown: {
+    renderer: {
+      image: function (href, title) {
+        return `<img src="${href}" data-origin="${href}" alt="${title}">`;
+      },
+    },
+  },
+};
