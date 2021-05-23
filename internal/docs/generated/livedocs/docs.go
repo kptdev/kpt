d19f09f7972ef74dbc45ece0d2e30a406fbc0838
NagaHarshita <44162497+NagaHarshita@users.noreply.github.com>
2021-05-24 03:53:23 +0530

    Docs: make generate (#2080)

--- internal/docs/generated/livedocs/docs.go
+++ internal/docs/generated/livedocs/docs.go
@@ -19,6 +19,14 @@ Args:
     Using '-' as the package path will cause kpt to read resources from stdin.
 
 Flags:
+  --dry-run:
+    It true, kpt will validate the resources in the package and print which
+    resources will be applied and which resources will be pruned, but no resources
+    will be changed.
+    If the --server-side flag is true, kpt will do a server-side dry-run, otherwise
+    it will be a client-side dry-run. Note that the output will differ somewhat
+    between the two alternatives. 
+  
   --field-manager:
     Identifier for the **owner** of the fields being applied. Only usable
     when --server-side flag is specified. Default value is kubectl.
@@ -92,6 +100,10 @@ Args:
     Using '-' as the package path will cause kpt to read resources from stdin.
 
 Flags:
+  --dry-run:
+    It true, kpt will print the resources that will be removed from the cluster, 
+    but no resources will be deleted.
+  
   --inventory-policy:
     Determines how to handle overlaps between the package being currently applied
     and existing resources in the cluster. The available options are:
@@ -118,40 +130,6 @@ Flags:
 var DestroyExamples = `
   # remove all resources in the current package from the cluster.
   kpt live destroy
-`
-
-var DiffShort = `Display the diff between the local package and the live cluster resources.`
-var DiffLong = `
-  kpt live diff [PKG_PATH | -]
-
-Args:
-  PKG_PATH | -:
-    Path to the local package which should be diffed against the cluster. It must
-    contain a Kptfile with inventory information. Defaults to the current working
-    directory.
-    Using '-' as the package path will cause kpt to read resources from stdin.
-
-Environment Variables:
-  KUBECTL_EXTERNAL_DIFF:
-    Commandline diffing tool ('diff; by default) that will be used to show
-    changes.
-    
-    # Use meld to show changes
-    KPT_EXTERNAL_DIFF=meld kpt live diff
-
-Exit statuses:
-  The following exit values are returned:
-  
-    * 0: No differences were found.
-    * 1: Differences were found.
-    * >1 kpt live or diff failed with an error.
-`
-var DiffExamples = `
-  # diff the config in the current directory against the live cluster resources.
-  kpt live diff
-  
-  # specify the local diff program to use.
-  export KUBECTL_EXTERNAL_DIFF=meld; kpt live diff my-dir
 `
 
 var InitShort = `Initialize a package with the information needed for inventory tracking.`
@@ -221,71 +199,6 @@ Flags:
 var MigrateExamples = `
   # Migrate the package in the current directory.
   kpt live migrate
-`
-
-var PreviewShort = `Preview the changes apply would make to the cluster`
-var PreviewLong = `
-  kpt live preview [PKG_PATH | -] [flags]
-
-Args:
-
-  PKG_PATH | -:
-    Path to the local package for which a preview of the operations of apply
-    or destroy should be displayed. It must contain a Kptfile with inventory
-    information. Defaults to the current working directory.
-    Using '-' as the package path will cause kpt to read resources from stdin.
-
-Flags:
-
-  --destroy:
-    If true, preview deletion of all resources.
-  
-  --field-manager:
-    Identifier for the **owner** of the fields being applied. Only usable
-    when --server-side flag is specified. Default value is kubectl.
-  
-  --force-conflicts:
-    Force overwrite of field conflicts during apply due to different field
-    managers. Only usable when --server-side flag is specified.
-    Default value is false (error and failure when field managers conflict).
-  
-  --install-resource-group:
-    Install the ResourceGroup CRD into the cluster if it isn't already
-    available. Default is false.
-  
-  --inventory-policy:
-    Determines how to handle overlaps between the package being currently applied
-    and existing resources in the cluster. The available options are:
-    
-      * strict: If any of the resources already exist in the cluster, but doesn't
-        belong to the current package, it is considered an error.
-      * adopt: If a resource already exist in the cluster, but belongs to a 
-        different package, it is considered an error. Resources that doesn't belong
-        to other packages are adopted into the current package.
-        
-    The default value is ` + "`" + `strict` + "`" + `.
-  
-  --output:
-    Determines the output format for the status information. Must be one of the following:
-    
-      * events: The output will be a list of the status events as they become available.
-      * json: The output will be a list of the status events as they become available,
-        each formatted as a json object.
-      * table: The output will be presented as a table that will be updated inline
-        as the status of resources become available.
-  
-    The default value is ‘events’.
-  
-  --server-side:
-    Perform the apply operation server-side rather than client-side.
-    Default value is false (client-side).
-`
-var PreviewExamples = `
-  # preview apply for the package in the current directory. 
-  kpt live preview
-
-  # preview destroy for a package in the my-dir directory.
-  kpt live preview --destroy my-dir
 `
 
 var StatusShort = `Display shows the status for the resources in the cluster`
