320d57fd3b50c9f07a7775940c41ca4ba7596ea8
Phani Teja Marupaka <pmarupaka@google.com>
2021-06-18 17:04:49 -0700

    Update generated docs

--- internal/docs/generated/fndocs/docs.go
+++ internal/docs/generated/fndocs/docs.go
@@ -257,7 +257,7 @@ var RenderExamples = `
 
 var SinkShort = `Write resources to a local directory`
 var SinkLong = `
-  kpt fn sink [DIR] [flags]
+  kpt fn sink DIR [flags]
   
   DIR:
     Path to a local directory to write resources to. The directory must not already exist.
@@ -266,8 +266,8 @@ var SinkExamples = `
   # read resources from DIR directory, execute my-fn on them and write the
   # output to DIR directory.
   $ kpt fn source DIR |
-    kpt fn eval - --image gcr.io/example.com/my-fn - |
-    kpt fn sink DIR
+    kpt fn eval - --image gcr.io/example.com/my-fn |
+    kpt fn sink NEW_DIR
 `
 
 var SourceShort = `Source resources from a local directory`
