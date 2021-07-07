148c67e6e39fef9ddd43b1b244459b44514abb59
Phani Teja Marupaka <pmarupaka@google.com>
2021-07-07 16:40:21 -0700

    generate docs

--- internal/docs/generated/fndocs/docs.go
+++ internal/docs/generated/fndocs/docs.go
@@ -10,14 +10,17 @@ using containerized functions.
 var DocShort = `Display the documentation for a function`
 var DocLong = `
 ` + "`" + `kpt fn doc` + "`" + ` invokes the function container with ` + "`" + `--help` + "`" + ` flag.
+If the function supports ` + "`" + `--help` + "`" + `, it will print the documentation to STDOUT.
+Otherwise, it will exit with non-zero exit code and print the error message to STDERR.
 
   kpt fn doc --image=IMAGE
 
 Flags:
 
-  --image, i (required flag)
-    If the function supports --help, it will print the documentation to STDOUT.
-    Otherwise, it will exit with non-zero exit code and print the error message to STDERR.
+  --image, i: (required flag)
+    Container image of the function e.g. ` + "`" + `gcr.io/kpt-fn/set-namespace:v0.1` + "`" + `.
+    For convenience, if full image path is not specified, ` + "`" + `gcr.io/kpt-fn/` + "`" + ` is added as default prefix.
+    e.g. instead of passing ` + "`" + `gcr.io/kpt-fn/set-namespace:v0.1` + "`" + ` you can pass ` + "`" + `set-namespace:v0.1` + "`" + `.
 `
 var DocExamples = `
   # display the documentation for image set-namespace:v0.1.1
