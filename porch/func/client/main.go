9ae49256a4db8da32675d127daca9cadbf55df83
Martin Maly <mmaly@google.com>
2022-02-02 12:38:39 -0800

    Simple gRPC kpt function runner service (#2711)
    Several functions are built into the server Docker image and are exec'd
    with input.
    Added simple client which can be used (in ./func directory):
    
    ```
    go run ./client \
      --address=192.168.8.202:9445 \
      --package=./config/ \
      --image=gcr.io/kpt-fn/set-namespace:v0.2.0 \
      -- \
      namespace=foo
    ```

---
+++ porch/func/client/main.go
@@ -0,0 +1,151 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	pb "github.com/GoogleContainerTools/kpt/porch/func/evaluator"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"sigs.k8s.io/kustomize/kyaml/kio"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+var (
+	addressFlag = flag.String("address", "localhost:9445", "FunctionEvaluator server address")
+	packageFlag = flag.String("package", "", "Source package")
+	imageFlag   = flag.String("image", "", "Image of the function to evaluate")
+)
+
+func main() {
+	flag.Parse()
+
+	if err := run(flag.Args()); err != nil {
+		fmt.Fprintf(os.Stderr, "unexpected error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string) error {
+	rl, err := createResourceList(args)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Request:\n\n%s\n", string(rl))
+
+	res, err := call(rl)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Response:\n\n%s\n\nLog:\n%s\n", string(res.ResourceList), string(res.Log))
+	return nil
+}
+
+func createResourceList(args []string) ([]byte, error) {
+	r := kio.LocalPackageReader{
+		PackagePath:        *packageFlag,
+		IncludeSubpackages: true,
+	}
+	nodes, err := r.Read()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read package: %w", err)
+	}
+
+	cfg, err := configmap(args)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create function config: %w", err)
+	}
+
+	b := bytes.NewBuffer(nil)
+	w := kio.ByteWriter{
+		Writer:                b,
+		KeepReaderAnnotations: true,
+		Style:                 0,
+		FunctionConfig:        cfg,
+		WrappingKind:          kio.ResourceListKind,
+		WrappingAPIVersion:    kio.ResourceListAPIVersion,
+	}
+
+	if err := w.Write(nodes); err != nil {
+		return nil, fmt.Errorf("failed to create serialized ResourceList: %w", err)
+	}
+
+	return b.Bytes(), nil
+}
+
+func call(rl []byte) (*pb.EvaluateFunctionResponse, error) {
+	cc, err := grpc.Dial(*addressFlag, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to %s: %w", *addressFlag, err)
+	}
+	defer cc.Close()
+
+	evaluator := pb.NewFunctionEvaluatorClient(cc)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	in := &pb.EvaluateFunctionRequest{
+		ResourceList: rl,
+		Image:        *imageFlag,
+	}
+
+	r, err := evaluator.EvaluateFunction(ctx, in)
+	if err != nil {
+		return nil, fmt.Errorf("function evaluation failed: %w", err)
+	} else {
+		return r, nil
+	}
+}
+
+func configmap(args []string) (*yaml.RNode, error) {
+	if len(args) == 0 {
+		return nil, nil
+	}
+
+	data := map[string]string{}
+	for _, a := range args {
+		split := strings.SplitN(a, "=", 2)
+		if len(split) != 2 {
+			return nil, fmt.Errorf("invalid config value: %q", a)
+		}
+		data[split[0]] = split[1]
+	}
+	node := yaml.NewMapRNode(&data)
+	if node == nil {
+		return nil, nil
+	}
+
+	// create ConfigMap resource to contain function config
+	configMap := yaml.MustParse(`
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: function-input
+data: {}
+`)
+	if err := configMap.PipeE(yaml.SetField("data", node)); err != nil {
+		return nil, err
+	}
+	return configMap, nil
+}
--- porch/func/server/server.go
+++ porch/func/server/server.go
@@ -15,20 +15,51 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
 
 	pb "github.com/GoogleContainerTools/kpt/porch/func/evaluator"
+	"github.com/GoogleContainerTools/kpt/porch/func/internal"
 	"google.golang.org/grpc"
+	"k8s.io/klog/v2"
+)
+
+var (
+	port      = flag.Int("port", 9445, "The server port")
+	functions = flag.String("functions", "./functions", "Path to cached functions.")
+	config    = flag.String("config", "./config.yaml", "Path to the config file.")
 )
 
 func main() {
-	lis, _ := net.Listen("tcp", fmt.Sprint(":9445"))
-	server := grpc.NewServer()
-	pb.RegisterFunctionEvaluatorServer(server, &evaluator{})
-	server.Serve(lis)
+	flag.Parse()
+
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "unexpected error: %v\n", err)
+		os.Exit(1)
+	}
 }
 
-type evaluator struct {
-	pb.UnimplementedFunctionEvaluatorServer
+func run() error {
+	address := fmt.Sprintf(":%d", *port)
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to listen: %w", err)
+	}
+
+	evaluator, err := internal.NewEvaluatorWithConfig(*functions, *config)
+	if err != nil {
+		return fmt.Errorf("failed to initialize evaluator server: %w", err)
+	}
+
+	klog.Infof("Listening on %s", address)
+
+	// Start the gRPC server
+	server := grpc.NewServer()
+	pb.RegisterFunctionEvaluatorServer(server, evaluator)
+	if err := server.Serve(lis); err != nil {
+		return fmt.Errorf("server failed: %w", err)
+	}
+	return nil
 }
