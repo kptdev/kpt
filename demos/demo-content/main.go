590241bce85ffafb91c156b47a99ca81d8679170
Louis DeJardin <dejardin@google.com>
2022-02-02 21:40:23 +0000

    updating demo usage to include parsed argument
    - enables passing an oci:// arg

--- demos/demo-content/main.go
+++ demos/demo-content/main.go
@@ -15,23 +15,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
 
+	"github.com/GoogleContainerTools/kpt/internal/printer"
 	"github.com/GoogleContainerTools/kpt/pkg/content/open"
 	"github.com/GoogleContainerTools/kpt/pkg/location"
 )
 
 func main() {
-	run("temp folder examples", tempFolderExamples)
+	// wire the global printer
+	pr := printer.New(os.Stdout, os.Stderr)
 
-}
+	// create context with associated printer
+	ctx := printer.WithContext(context.Background(), pr)
+	
+	// run through samples on location.Dir{}
+	fmt.Print("\n## Temp folder example\n\n")
+	if err := tempFolderExample(ctx); err != nil {
+		fmt.Printf("error in tempFolderExamples: %v\n", err)
+	}
 
-func run(caption string, example func() error) {
-	if err := example(); err != nil {
-		fmt.Printf("error in %q: %v\n", caption, err)
+	// run through examples on location.Git{}
+	fmt.Print("\n## GitHub blueprint example\n\n")
+	if err := githubBlueprintExample(ctx); err != nil {
+		fmt.Printf("error in githubBlueprintExample: %v\n", err)
+	}
+	
+	if len(os.Args) == 2 {
+		// run through examples on provided location
+		fmt.Print("\n## Parsed location example\n\n")
+		if err := locationExample(ctx, os.Args[1]); err != nil {
+			fmt.Printf("error in locationExample: %v\n", err)
+		}
 	}
 }
 
@@ -44,7 +63,7 @@ spec:
   target: world
 `
 
-func tempFolderExamples() error {
+func tempFolderExample(ctx context.Context) error {
 	// set up temp dir
 	tmp, err := os.MkdirTemp("", "")
 	if err != nil {
@@ -52,7 +71,7 @@ func tempFolderExamples() error {
 	}
 	defer os.RemoveAll(tmp)
 
-	if err := os.WriteFile(filepath.Join(tmp, "hello.txt"), []byte("world"), os.ModePerm); err != nil {
+	if err := os.WriteFile(filepath.Join(tmp, "README.md"), []byte("Hello world"), os.ModePerm); err != nil {
 		return err
 	}
 	if err := os.WriteFile(filepath.Join(tmp, "hello.yaml"), []byte(helloYaml), os.ModePerm); err != nil {
@@ -60,31 +79,56 @@ func tempFolderExamples() error {
 	}
 
 	return runAll(
+		ctx,
 		location.Dir{
 			Directory: tmp,
 		},
 	)
 }
 
-func runAll(ref location.Reference) error {
-	if err := openContent(ref); err != nil {
+func githubBlueprintExample(ctx context.Context) error {
+	return runAll(
+		ctx,
+		location.Git{
+			Repo: "https://github.com/GoogleCloudPlatform/blueprints",
+			Directory: "catalog/gke",
+			Ref: "main",
+		},
+	)
+}
+
+func locationExample(ctx context.Context, arg string) error {
+	ref, err := location.ParseReference(
+		arg,
+		location.WithContext(ctx),
+		location.WithParsers(location.OciParser, location.GitParser, location.DirParser),
+	)
+	if err != nil {
+		return err
+	}
+
+	return runAll(ctx, ref)
+}
+
+func runAll(ctx context.Context, ref location.Reference) error {
+	if err := openContent(ctx, ref); err != nil {
 		return fmt.Errorf("open content: %v", err)
 	}
-	if err := readFromFS(ref); err != nil {
-		return fmt.Errorf("open content as FS: %v", err)
-	}
-	if err := readFromFileSystem(ref); err != nil {
+	if err := readFromFileSystem(ctx, ref); err != nil {
 		return fmt.Errorf("open content as FileSystem: %v", err)
 	}
-	if err := readFromReader(ref); err != nil {
+	if err := readFromReader(ctx, ref); err != nil {
 		return fmt.Errorf("open content as Reader: %v", err)
+	}
+	if err := readFromFS(ctx, ref); err != nil {
+		return fmt.Errorf("open content as FS: %v", err)
 	}
 	return nil
 }
 
-func openContent(ref location.Reference) error {
+func openContent(ctx context.Context, ref location.Reference) error {
 	// open it as content
-	src, err := open.Content(ref)
+	src, err := open.Content(ref, open.WithContext(ctx))
 	if err != nil {
 		return err
 	}
@@ -94,41 +138,48 @@ func openContent(ref location.Reference) error {
 	return nil
 }
 
-func readFromFS(ref location.Reference) error {
+func readFromFS(ctx context.Context, ref location.Reference) error {
 	// open it as content as well as FS
-	src, err := open.FS(ref)
+	src, err := open.FS(ref, open.WithContext(ctx))
 	if err != nil {
 		return err
 	}
 	defer src.Close()
 
-	b, err := fs.ReadFile(src.FS, "hello.txt")
+	b, err := fs.ReadFile(src.FS, "README.md")
 	if err != nil {
 		return err
 	}
-	fmt.Printf("read %q\n", b)
+	fmt.Printf("read %q\n", short(b, 30))
 	return nil
 }
 
-func readFromFileSystem(ref location.Reference) error {
+func readFromFileSystem(ctx context.Context, ref location.Reference) error {
 	// open it as content as well as FS
-	src, err := open.FileSystem(ref)
+	src, err := open.FileSystem(ref, open.WithContext(ctx))
 	if err != nil {
 		return err
 	}
 	defer src.Close()
 
-	b, err := src.FileSystem.ReadFile(filepath.Join(src.Path, "hello.txt"))
+	b, err := src.FileSystem.ReadFile(filepath.Join(src.Path, "README.md"))
 	if err != nil {
 		return err
 	}
-	fmt.Printf("read %q\n", b)
+	fmt.Printf("read %q\n", short(b, 30))
 	return nil
 }
 
-func readFromReader(ref location.Reference) error {
+func short(b []byte, n int) []byte {
+	if len(b) < n {
+		return b
+	}
+	return b[0:n]
+}
+
+func readFromReader(ctx context.Context, ref location.Reference) error {
 	// open it as content as well as FS
-	src, err := open.Reader(ref)
+	src, err := open.Reader(ref, open.WithContext(ctx))
 	if err != nil {
 		return err
 	}
@@ -139,8 +190,10 @@ func readFromReader(ref location.Reference) error {
 		return err
 	}
 	for _, node := range nodes {
-		s, _ := node.String()
-		fmt.Printf("read %v\n", s)
+		//s, _ := node.String()
+		m, _ := node.GetMeta()
+		
+		fmt.Printf("read %v\n", m.GetIdentifier())
 	}
 	return nil
 }
