31405745cf2733444b53da5e25bec60466171845
Justin Santa Barbara <justinsb@google.com>
2022-03-01 13:09:36 -0500

    porch: create simple standalone app (#2830)
    We need an app that doesn't have validators etc.

---
+++ porch/config/samples/apps/hello-server/main.go
@@ -0,0 +1,45 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	http.HandleFunc("/", HelloHandler)
+
+	listen := ":8080"
+	if err := http.ListenAndServe(listen, nil); err != nil {
+		return fmt.Errorf("error listening on %q: %w", listen, err)
+	}
+
+	// This is documented not to happen
+	return fmt.Errorf("error:: ListenAndServe returned nil error")
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world\n")
+}
