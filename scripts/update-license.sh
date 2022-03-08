123d53d91137ba260741c29931d04595363f2184
droot <sunilarora@google.com>
2022-03-07 16:20:04 -0800

    update license script

--- scripts/update-license.sh
+++ scripts/update-license.sh
@@ -19,5 +19,6 @@ find . -type f \
 ! -path "./site/*" \
 ! -path "./docs/*" \
 ! -path "**/.expected/results.yaml" \
+! -path "**/testData/*" \
 ! -path "**/generated/*" \
 | xargs $GOBIN/addlicense -y 2021 -l apache
