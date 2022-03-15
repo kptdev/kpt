cfa917b52eebf2ae0a109dd7a00811a54361a26b
droot <sunilarora@google.com>
2022-03-15 13:20:17 -0700

    adjusted the tests to new behavior

--- e2e/testdata/fn-eval/fn-source/exclude-meta-resources-by-default/.expected/exec.sh
+++ e2e/testdata/fn-eval/fn-source/exclude-meta-resources-by-default/.expected/exec.sh
@@ -18,5 +18,5 @@ set -eo pipefail
 rm -rf out
 
 kpt fn source \
-| kpt fn eval - --image gcr.io/kpt-fn/set-namespace:v0.1.3 -- namespace=staging \
+| kpt fn eval - --image gcr.io/kpt-fn/set-namespace:v0.2.0 -- namespace=staging \
 | kpt fn sink out
