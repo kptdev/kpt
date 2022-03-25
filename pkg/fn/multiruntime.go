aa8a711ca7000c18a47f84159aa0f604c65c4909
Justin SB <justinsb@google.com>
2022-03-25 09:57:46 -0400

    Create multi-runtime capability
    Better define the error when a function is not found, and use this to
    support multiple engines.

---
+++ pkg/fn/multiruntime.go
@@ -0,0 +1,55 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fn
+
+import (
+	"context"
+	"errors"
+
+	v1 "github.com/GoogleContainerTools/kpt/pkg/api/kptfile/v1"
+)
+
+// MultiRuntime is a compound FunctionRuntime that will use the first available runner.
+type MultiRuntime struct {
+	runtimes []FunctionRuntime
+}
+
+// NewMultiRuntime builds a MultiRuntime.
+func NewMultiRuntime(runtimes []FunctionRuntime) *MultiRuntime {
+	return &MultiRuntime{
+		runtimes: runtimes,
+	}
+}
+
+var _ FunctionRuntime = &MultiRuntime{}
+
+// GetRunner implements FunctionRuntime
+func (r *MultiRuntime) GetRunner(ctx context.Context, fn *v1.Function) (FunctionRunner, error) {
+	for _, runtime := range r.runtimes {
+		runner, err := runtime.GetRunner(ctx, fn)
+		if err != nil {
+			var notFoundError *NotFoundError
+			if errors.As(err, &notFoundError) {
+				// maybe another runtime
+			} else {
+				return nil, err
+			}
+		} else {
+			return runner, nil
+		}
+	}
+
+	return nil, &NotFoundError{Function: *fn}
+}
