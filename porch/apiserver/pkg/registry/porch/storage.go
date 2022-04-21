2df4f165ddac6613b7b8b6f10427d5a5e0f24dba
Martin Maly <mmaly@google.com>
2022-04-21 14:50:09 -0700

    Custom table rendering of Package resources (#3039)
    Create custom table rendering of PackageRevision and
    PackageRevisionResources lists.

--- porch/apiserver/pkg/registry/porch/storage.go
+++ porch/apiserver/pkg/registry/porch/storage.go
@@ -28,7 +28,7 @@ import (
 
 func NewRESTStorage(scheme *runtime.Scheme, codecs serializer.CodecFactory, cad engine.CaDEngine, coreClient client.WithWatch) (genericapiserver.APIGroupInfo, error) {
 	packageRevisions := &packageRevisions{
-		TableConvertor: rest.NewDefaultTableConvertor(porch.Resource("packagerevisions")),
+		TableConvertor: packageRevisionTableConvertor,
 		packageCommon: packageCommon{
 			cad:            cad,
 			gr:             porch.Resource("packagerevisions"),
@@ -47,7 +47,7 @@ func NewRESTStorage(scheme *runtime.Scheme, codecs serializer.CodecFactory, cad 
 	}
 
 	packageRevisionResources := &packageRevisionResources{
-		TableConvertor: rest.NewDefaultTableConvertor(porch.Resource("packagerevisionresources")),
+		TableConvertor: packageRevisionResourcesTableConvertor,
 		packageCommon: packageCommon{
 			cad:        cad,
 			gr:         porch.Resource("packagerevisionresources"),
