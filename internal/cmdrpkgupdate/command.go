0e1b83298e3e9db2a5241b556f07bd6e6c5590d0
Natasha Sarkar <natashasarkar@google.com>
2022-06-24 16:01:38 -0700

    flag to `rpkg update` to discover available updates (#3290)

--- internal/cmdrpkgupdate/command.go
+++ internal/cmdrpkgupdate/command.go
@@ -46,6 +46,8 @@ func newRunner(ctx context.Context, rcg *genericclioptions.ConfigFlags) *runner 
 		Hidden:  porch.HidePorchCommands,
 	}
 	r.Command.Flags().StringVar(&r.revision, "revision", "", "Revision of the upstream package to update to.")
+	r.Command.Flags().BoolVar(&r.discover, "discover", false,
+		"If set, search for available updates instead of performing an update. If no arguments given, look at all package revisions.")
 	return r
 }
 
@@ -56,27 +58,39 @@ type runner struct {
 	Command *cobra.Command
 
 	revision string // Target package revision
+	discover bool   // If set, discover updates rather than do updates
+
+	// there are multiple places where we need access to all package revisions, so
+	// we store it in the runner
+	prs []porchapi.PackageRevision
 }
 
 func (r *runner) preRunE(cmd *cobra.Command, args []string) error {
 	const op errors.Op = command + ".preRunE"
-	client, err := porch.CreateClient(r.cfg)
+	c, err := porch.CreateClient(r.cfg)
 	if err != nil {
 		return errors.E(op, err)
 	}
-	r.client = client
+	r.client = c
 
-	if len(args) < 1 {
-		return errors.E(op, fmt.Errorf("SOURCE_PACKAGE is a required positional argument"))
-	}
-	if len(args) > 1 {
-		return errors.E(op, fmt.Errorf("too many arguments; SOURCE_PACKAGE is the only accepted positional arguments"))
+	if !r.discover {
+		if len(args) < 1 {
+			return errors.E(op, fmt.Errorf("SOURCE_PACKAGE is a required positional argument"))
+		}
+		if len(args) > 1 {
+			return errors.E(op, fmt.Errorf("too many arguments; SOURCE_PACKAGE is the only accepted positional arguments"))
+		}
+		// TODO: This should use the latest available revision if one isn't specified.
+		if r.revision == "" {
+			return errors.E(op, fmt.Errorf("revision is required"))
+		}
 	}
 
-	// TODO: This should use the latest available revision if one isn't specified.
-	if r.revision == "" {
-		return errors.E(op, fmt.Errorf("revision is required"))
+	packageRevisionList := porchapi.PackageRevisionList{}
+	if err := r.client.List(r.ctx, &packageRevisionList, &client.ListOptions{}); err != nil {
+		return errors.E(op, err)
 	}
+	r.prs = packageRevisionList.Items
 
 	return nil
 }
@@ -84,51 +98,53 @@ func (r *runner) preRunE(cmd *cobra.Command, args []string) error {
 func (r *runner) runE(cmd *cobra.Command, args []string) error {
 	const op errors.Op = command + ".runE"
 
-	var pr porchapi.PackageRevision
-	err := r.client.Get(r.ctx, client.ObjectKey{
-		Namespace: *r.cfg.Namespace,
-		Name:      args[0],
-	}, &pr)
+	var err error
+	if r.discover {
+		err = r.discoverUpdates(cmd, args)
+	} else {
+		pr := r.findPackageRevision(args[0])
+		if pr == nil {
+			return errors.E(op, fmt.Errorf("could not find package revision %s", args[0]))
+		}
+		err = r.doUpdate(pr)
+	}
 	if err != nil {
 		return errors.E(op, err)
 	}
+	return nil
+}
 
-	cloneTask, found := r.findCloneTask(&pr)
+func (r *runner) doUpdate(pr *porchapi.PackageRevision) error {
+	cloneTask, found := r.findCloneTask(pr)
 	if !found {
-		err := fmt.Errorf("upstream source not found. Only cloned packages can be updated")
-		return errors.E(op, err)
+		return fmt.Errorf("upstream source not found. Only cloned packages can be updated")
 	}
 
 	switch cloneTask.Clone.Upstream.Type {
 	case porchapi.RepositoryTypeGit:
 		cloneTask.Clone.Upstream.Git.Ref = r.revision
 	case porchapi.RepositoryTypeOCI:
-		err := fmt.Errorf("update not implemented for oci packages")
-		return errors.E(op, err)
+		return fmt.Errorf("update not implemented for oci packages")
 	default:
-		upstreamPr, err := r.findPackageRevision(cloneTask.Clone.Upstream.UpstreamRef.Name)
-		if err != nil {
-			err := fmt.Errorf("error fetch package revisions: %w", err)
-			return errors.E(op, err)
-		}
+		upstreamPr := r.findPackageRevision(cloneTask.Clone.Upstream.UpstreamRef.Name)
 		if upstreamPr == nil {
-			err := fmt.Errorf("upstream package revision %s no longer exists", cloneTask.Clone.Upstream.UpstreamRef.Name)
-			return errors.E(op, err)
+			return fmt.Errorf("upstream package revision %s no longer exists", cloneTask.Clone.Upstream.UpstreamRef.Name)
 		}
-		newUpstreamPr, err := r.findPackageRevisionForRef(upstreamPr.Spec.PackageName)
-		if err != nil {
-			err := fmt.Errorf("error fetching package revisions: %w", err)
-			return errors.E(op, err)
-		}
+		newUpstreamPr := r.findPackageRevisionForRef(upstreamPr.Spec.PackageName)
 		if newUpstreamPr == nil {
-			err := fmt.Errorf("revision %s does not exist for package %s", r.revision, pr.Spec.PackageName)
-			return errors.E(op, err)
+			return fmt.Errorf("revision %s does not exist for package %s", r.revision, pr.Spec.PackageName)
 		}
 		cloneTask.Clone.Upstream.UpstreamRef.Name = newUpstreamPr.Name
 	}
+	return r.client.Update(r.ctx, pr)
+}
 
-	if err := r.client.Update(r.ctx, &pr); err != nil {
-		return errors.E(op, err)
+func (r *runner) findPackageRevision(prName string) *porchapi.PackageRevision {
+	for i := range r.prs {
+		pr := r.prs[i]
+		if pr.Name == prName {
+			return &pr
+		}
 	}
 	return nil
 }
@@ -143,32 +159,12 @@ func (r *runner) findCloneTask(pr *porchapi.PackageRevision) (*porchapi.Task, bo
 	return nil, false
 }
 
-func (r *runner) findPackageRevision(prName string) (*porchapi.PackageRevision, error) {
-	var prList porchapi.PackageRevisionList
-	if err := r.client.List(r.ctx, &prList, &client.ListOptions{}); err != nil {
-		return nil, err
-	}
-
-	for i := range prList.Items {
-		pr := prList.Items[i]
-		if pr.Name == prName {
-			return &pr, nil
-		}
-	}
-	return nil, nil
-}
-
-func (r *runner) findPackageRevisionForRef(name string) (*porchapi.PackageRevision, error) {
-	var prList porchapi.PackageRevisionList
-	if err := r.client.List(r.ctx, &prList, &client.ListOptions{}); err != nil {
-		return nil, err
-	}
-
-	for i := range prList.Items {
-		pr := prList.Items[i]
+func (r *runner) findPackageRevisionForRef(name string) *porchapi.PackageRevision {
+	for i := range r.prs {
+		pr := r.prs[i]
 		if pr.Spec.PackageName == name && pr.Spec.Revision == r.revision {
-			return &pr, nil
+			return &pr
 		}
 	}
-	return nil, nil
+	return nil
 }
---
+++ internal/cmdrpkgupdate/discover.go
@@ -0,0 +1,147 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmdrpkgupdate
+
+import (
+	"fmt"
+	"strings"
+
+	porchapi "github.com/GoogleContainerTools/kpt/porch/api/porch/v1alpha1"
+	configapi "github.com/GoogleContainerTools/kpt/porch/api/porchconfig/v1alpha1"
+	"github.com/spf13/cobra"
+	"golang.org/x/mod/semver"
+	"k8s.io/cli-runtime/pkg/printers"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func (r *runner) discoverUpdates(cmd *cobra.Command, args []string) error {
+	var prs []porchapi.PackageRevision
+	var errs []string
+	if len(args) == 0 {
+		prs = r.prs
+	} else {
+		for i := range args {
+			pr := r.findPackageRevision(args[i])
+			if pr == nil {
+				errs = append(errs, fmt.Sprintf("could not find package revision %s", args[i]))
+				continue
+			}
+			prs = append(prs, *pr)
+		}
+	}
+	if len(errs) > 0 {
+		return fmt.Errorf("errors:\n  %s", strings.Join(errs, "\n  "))
+	}
+
+	repositories, err := r.getRepositories()
+	if err != nil {
+		return err
+	}
+
+	var updates [][]string
+	for _, pr := range prs {
+		availableUpdates, upstreamName := r.availableUpdates(pr.Status.UpstreamLock, repositories)
+		if len(availableUpdates) == 0 {
+			updates = append(updates, []string{pr.Name, upstreamName, "No update available"})
+		} else {
+			updates = append(updates, []string{pr.Name, upstreamName, strings.Join(availableUpdates, ", ")})
+		}
+	}
+
+	w := printers.GetNewTabWriter(cmd.OutOrStdout())
+	if _, err := fmt.Fprintln(w, "PACKAGE REVISION\tUPSTREAM REPOSITORY\tUPSTREAM UPDATES"); err != nil {
+		return err
+	}
+	for _, pkgRev := range updates {
+		if _, err := fmt.Fprintln(w, strings.Join(pkgRev, "\t")); err != nil {
+			return err
+		}
+	}
+
+	return w.Flush()
+}
+
+func (r *runner) availableUpdates(upstreamLock *porchapi.UpstreamLock, repositories *configapi.RepositoryList) ([]string, string) {
+	var availableUpdates []string
+	var upstream string
+
+	if upstreamLock == nil || upstreamLock.Git == nil {
+		return nil, ""
+	}
+	// separate the revision number from the package name
+	lastIndex := strings.LastIndex(upstreamLock.Git.Ref, "v")
+	if lastIndex < 0 {
+		return nil, ""
+	}
+	currentUpstreamRevision := upstreamLock.Git.Ref[lastIndex:]
+
+	// upstream.git.ref could look like drafts/pkgname/version or pkgname/version
+	upstreamPackageName := upstreamLock.Git.Ref[:lastIndex-1]
+	upstreamPackageName = strings.TrimPrefix(upstreamPackageName, "drafts/")
+
+	if !strings.HasSuffix(upstreamLock.Git.Repo, ".git") {
+		upstreamLock.Git.Repo += ".git"
+	}
+
+	// find a repo that matches the upstreamLock
+	var revisions []string
+	for _, repo := range repositories.Items {
+		if repo.Spec.Type != configapi.RepositoryTypeGit {
+			// we are not currently supporting non-git repos for updates
+			continue
+		}
+		if !strings.HasSuffix(repo.Spec.Git.Repo, ".git") {
+			repo.Spec.Git.Repo += ".git"
+		}
+		if upstreamLock.Git.Repo == repo.Spec.Git.Repo {
+			upstream = repo.Name
+			revisions = r.getUpstreamRevisions(repo, upstreamPackageName)
+		}
+	}
+
+	for _, upstreamRevision := range revisions {
+		switch cmp := semver.Compare(upstreamRevision, currentUpstreamRevision); {
+		case cmp > 0: // upstreamRevision > currentUpstreamRevision
+			availableUpdates = append(availableUpdates, upstreamRevision)
+		case cmp == 0, cmp < 0: // upstreamRevision <= currentUpstreamRevision, do nothing
+		}
+	}
+
+	return availableUpdates, upstream
+}
+
+// fetches all registered repositories
+func (r *runner) getRepositories() (*configapi.RepositoryList, error) {
+	repoList := configapi.RepositoryList{}
+	err := r.client.List(r.ctx, &repoList, &client.ListOptions{})
+	return &repoList, err
+}
+
+// fetches all package revision numbers for packages with the name upstreamPackageName from the repo
+func (r *runner) getUpstreamRevisions(repo configapi.Repository, upstreamPackageName string) []string {
+	var result []string
+	for _, pkgRev := range r.prs {
+		if pkgRev.Spec.Lifecycle != porchapi.PackageRevisionLifecyclePublished {
+			// only consider published packages
+			continue
+		}
+		if pkgRev.Spec.RepositoryName != repo.Name ||
+			pkgRev.Spec.PackageName != upstreamPackageName {
+			continue
+		}
+		result = append(result, pkgRev.Spec.Revision)
+	}
+	return result
+}
