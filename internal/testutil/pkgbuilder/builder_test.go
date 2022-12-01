7fabc8fd760edc4a93b06e914f304d7c5911fa9a
Justin SB <justinsb@google.com>
2022-12-01 08:16:39 -0500

    Add simple test for buildKptfile
    Sets us up to replace the use of text/template

---
+++ internal/testutil/pkgbuilder/builder_test.go
@@ -0,0 +1,38 @@
+package pkgbuilder
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestBuildKptfile(t *testing.T) {
+	var repos ReposInfo
+	pkg := &pkg{}
+	pkg.Kptfile = &Kptfile{
+		Pipeline: &Pipeline{
+			Functions: []Function{
+				{Image: "example.com/fn1"},
+				{ConfigPath: "config1"},
+				{Image: "example.com/fn2"},
+			},
+		},
+	}
+
+	got := buildKptfile(pkg, "test1", repos)
+	want := `apiVersion: kpt.dev/v1
+kind: Kptfile
+metadata:
+  name: test1
+pipeline:
+  mutators:
+  - image: example.com/fn1
+  - image: 
+  - configPath: config1
+  - image: example.com/fn2
+`
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("buildKptfile returned unexpected diff (-want +got):\n%s", diff)
+	}
+}
