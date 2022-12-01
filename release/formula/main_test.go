07aff759aa2ff44b9e06740875c5f39d2e051783
Justin SB <justinsb@google.com>
2022-12-01 07:45:50 -0500

    brew formula: replace text/template
    Also add a test while we're at it.

---
+++ release/formula/main_test.go
@@ -0,0 +1,65 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFormula(t *testing.T) {
+	want := `# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class Kpt < Formula
+  desc "Toolkit to manage,and apply Kubernetes Resource config data files"
+  homepage "https://googlecontainertools.github.io/kpt"
+  url "https://github.com/GoogleContainerTools/kpt/archive/v1.0.0-beta.23.tar.gz"
+  sha256 "45c34e7f6fb8246dbe1b35769d988fe18c64d668b2fba747d45fd1e22eacf99b"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GO111MODULE"] = "on"
+    system "go", "build", "-ldflags", "-X github.com/GoogleContainerTools/kpt/run.version=#{version}", *std_go_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/kpt version")
+  end
+end
+`
+
+	url := "https://github.com/GoogleContainerTools/kpt/archive/v1.0.0-beta.23.tar.gz"
+	got, err := buildFormula(url)
+	if err != nil {
+		t.Fatalf("error from buildFormula(%q): %v", url, err)
+	}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("buildFormula(%q) returned unexpected diff (-want +got):\n%s", url, diff)
+	}
+}
