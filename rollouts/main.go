c07d23c1a701b812ae3994f3625f0a3142b6aa30
Sunil Arora <sunilarora@google.com>
2022-12-21 14:24:56 -0800

    rollouts: added remoterootsync API (#3698)

--- rollouts/main.go
+++ rollouts/main.go
@@ -96,6 +96,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Rollout")
 		os.Exit(1)
 	}
+	if err = (&controllers.RemoteRootSyncReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "RemoteRootSync")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
