9b66acd8d7d878a90edb54dcad942fa7ddc76f35
Sunil Arora <sunilarora@google.com>
2023-01-11 14:26:21 -0800

    rollouts: added skeleton CLI (#3724)
    * rollouts: added skeleton CLI
    
    * added table display

---
+++ rollouts/cli/status/status.go
@@ -0,0 +1,86 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package status
+
+import (
+	"context"
+	"fmt"
+
+	rolloutsapi "github.com/GoogleContainerTools/kpt/rollouts/api/v1alpha1"
+	"github.com/GoogleContainerTools/kpt/rollouts/rolloutsclient"
+	"github.com/spf13/cobra"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+)
+
+func newRunner(ctx context.Context) *runner {
+	r := &runner{
+		ctx: ctx,
+	}
+	c := &cobra.Command{
+		Use:     "status",
+		Short:   "displays status of a rollout",
+		Long:    "displays status of a rollout",
+		Example: "displays status of a rollout",
+		RunE:    r.runE,
+	}
+	r.Command = c
+	return r
+}
+
+func NewCommand(ctx context.Context) *cobra.Command {
+	return newRunner(ctx).Command
+}
+
+type runner struct {
+	ctx     context.Context
+	Command *cobra.Command
+}
+
+func (r *runner) runE(cmd *cobra.Command, args []string) error {
+	rlc, err := rolloutsclient.New()
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return err
+	}
+
+	if len(args) == 0 {
+		fmt.Printf("must provide rollout name")
+		return nil
+	}
+
+	rollout, err := rlc.Get(r.ctx, args[0])
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return err
+	}
+
+	renderStatusAsTable(cmd, rollout)
+	return nil
+}
+
+func renderStatusAsTable(cmd *cobra.Command, rollout *rolloutsapi.Rollout) {
+	t := table.NewWriter()
+	t.SetOutputMirror(cmd.OutOrStdout())
+	t.AppendHeader(table.Row{"CLUSTER", "PACKAGE ID", "PACKAGE STATUS", "SYNC STATUS"})
+	for _, cluster := range rollout.Status.ClusterStatuses {
+		pkgStatus := cluster.PackageStatus
+		t.AppendRow([]interface{}{cluster.Name, pkgStatus.PackageID, pkgStatus.Status, pkgStatus.SyncStatus})
+	}
+	t.AppendSeparator()
+	// t.AppendRow([]interface{}{300, "Tyrion", "Lannister", 5000})
+	// t.AppendFooter(table.Row{"", "", "Total", 10000})
+	t.Render()
+}
---
+++ rollouts/rolloutsclient/client.go
@@ -0,0 +1,92 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rolloutsclient
+
+import (
+	"context"
+	"fmt"
+
+	rolloutsapi "github.com/GoogleContainerTools/kpt/rollouts/api/v1alpha1"
+	coreapi "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+)
+
+// Client implments client for the rollouts API.
+type Client struct {
+	client client.Client
+}
+
+func New() (*Client, error) {
+	scheme, err := createScheme()
+	if err != nil {
+		return nil, err
+	}
+	cl, err := client.New(config.GetConfigOrDie(), client.Options{
+		Scheme: scheme,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &Client{client: cl}, nil
+}
+
+func createScheme() (*runtime.Scheme, error) {
+	scheme := runtime.NewScheme()
+
+	for _, api := range (runtime.SchemeBuilder{
+		rolloutsapi.AddToScheme,
+		coreapi.AddToScheme,
+		metav1.AddMetaToScheme,
+	}) {
+		if err := api(scheme); err != nil {
+			return nil, err
+		}
+	}
+	return scheme, nil
+}
+
+func (rlc *Client) List(ctx context.Context, ns string) (*rolloutsapi.RolloutList, error) {
+	if ns == "" {
+		ns = "default"
+	}
+
+	rollouts := &rolloutsapi.RolloutList{}
+	if err := rlc.client.List(context.Background(), rollouts, client.InNamespace(ns)); err != nil {
+		return nil, err
+	}
+
+	return rollouts, nil
+}
+
+func (rlc *Client) Get(ctx context.Context, name string) (*rolloutsapi.Rollout, error) {
+	if name == "" {
+		return nil, fmt.Errorf("must provide rollout name")
+	}
+
+	key := types.NamespacedName{
+		Namespace: "default",
+		Name:      name,
+	}
+	rollout := &rolloutsapi.Rollout{}
+	if err := rlc.client.Get(context.Background(), key, rollout); err != nil {
+		return nil, err
+	}
+
+	return rollout, nil
+}
