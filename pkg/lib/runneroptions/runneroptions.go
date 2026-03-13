571beaf2c34712fa9950a218662dadcdc9c3a18d
Mózes László Máté <laszlo.mozes@nokia.com>
2026-03-13 17:08:12 +0100

    address copilot comments
    Signed-off-by: Mózes László Máté <laszlo.mozes@nokia.com>

--- pkg/lib/runneroptions/runneroptions.go
+++ pkg/lib/runneroptions/runneroptions.go
@@ -67,8 +67,8 @@ func (opts *RunnerOptions) InitDefaults(defaultImagePrefix string) {
 // ResolveToImageForCLIFunc returns a func that converts the KRM function short path to the full image url.
 // If the function is a catalog function, it prepends `prefix`, e.g. "set-namespace:v0.1" --> prefix + "set-namespace:v0.1".
 // A "/" is appended to `prefix` if it is not an empty string and does not end with a "/".
-func ResolveToImageForCLIFunc(prefix string) func(image string) string {
-	prefix = strings.TrimSuffix(prefix, "/")
+func ResolveToImageForCLIFunc(prefix string) ImageResolveFunc {
+	prefix = strings.TrimRight(prefix, "/")
 	if prefix == "" {
 		return func(image string) string {
 			return image
