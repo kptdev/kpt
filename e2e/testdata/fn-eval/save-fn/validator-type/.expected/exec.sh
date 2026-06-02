e8580da53bd36d9156e4c85d499e146f735d8339
Aravindhan Ayyanathan <aravindhan.a@est.tech>
2026-06-02 10:08:21 +0100

    Update copyright and fix set-namespace exit code.
    Signed-off-by: Aravindhan Ayyanathan <aravindhan.a@est.tech>

--- e2e/testdata/fn-eval/save-fn/validator-type/.expected/exec.sh
+++ e2e/testdata/fn-eval/save-fn/validator-type/.expected/exec.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Copyright 2021-2026 The kpt Authors
+# Copyright 2021, 2026 The kpt Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
