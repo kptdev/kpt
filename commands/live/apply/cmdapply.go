8d861ebb959fd39fb153f4d06e679442447df6ee
OisinJohnston2005 <oisin.johnston@ericsson.com>
2026-07-22 10:42:37 +0100

    Add a check for namespace existence in kpt live apply (#4653)
    * Fixed #2123 "kpt live with dry-run does not recognize situations where the inventory namespace does not exist"
    
    Signed-off-by: Oisin Johnston <oisin.johnston@ericsson.com>
    
    * added tests
    
    Signed-off-by: Oisin Johnston <oisin.johnston@ericsson.com>
    
    ---------
    
    Signed-off-by: Oisin Johnston <oisin.johnston@ericsson.com>

--- commands/live/apply/cmdapply.go
+++ commands/live/apply/cmdapply.go
@@ -28,9 +28,11 @@ import (
 	alphaprinterstable "github.com/kptdev/kpt/pkg/printer/table"
 	"github.com/kptdev/kpt/pkg/status"
 	"github.com/spf13/cobra"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/kubectl/pkg/cmd/util"
 	"sigs.k8s.io/cli-utils/cmd/flagutils"
 	"sigs.k8s.io/cli-utils/pkg/apply"
@@ -53,6 +55,13 @@ func NewRunner(
 		factory:     factory,
 		applyRunner: runApply,
 		alpha:       alpha,
+	}
+	r.namespaceChecker = func(namespace string) error {
+		clientset, err := factory.KubernetesClientSet()
+		if err != nil {
+			return err
+		}
+		return checkNamespaceExists(clientset, namespace)
 	}
 	c := &cobra.Command{
 		Use:     "apply [PKG_PATH | -]",
@@ -124,6 +133,7 @@ type Runner struct {
 
 	applyRunner func(r *Runner, invInfo inventory.Info, objs []*unstructured.Unstructured,
 		dryRunStrategy common.DryRunStrategy) error
+	namespaceChecker func(namespace string) error
 }
 
 func (r *Runner) preRunE(cmd *cobra.Command, _ []string) error {
@@ -189,6 +199,12 @@ func (r *Runner) runE(c *cobra.Command, args []string) error {
 	objs, err = live.Flatten(objs)
 	if err != nil {
 		return err
+	}
+
+	if !live.NamespaceInObjects(objs, inv.Namespace) {
+		if err := r.namespaceChecker(inv.Namespace); err != nil {
+			return err
+		}
 	}
 
 	invInfo, err := live.ToInventoryInfo(inv)
@@ -288,4 +304,15 @@ func runApply(r *Runner, invInfo inventory.Info, objs []*unstructured.Unstructur
 		printer = printers.GetPrinter(r.output, r.ioStreams)
 	}
 	return printer.Print(ch, dryRunStrategy, r.printStatusEvents)
+}
+
+func checkNamespaceExists(clientset kubernetes.Interface, namespace string) error {
+	if namespace == "" {
+		namespace = "default"
+	}
+	_, err := clientset.CoreV1().Namespaces().Get(context.Background(), namespace, metav1.GetOptions{})
+	if apierrors.IsNotFound(err) {
+		return fmt.Errorf("inventory namespace %q does not exist", namespace)
+	}
+	return err
 }
