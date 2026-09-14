7d11e7f0b571f52c89b8c7fae362eb8bb4f3f4b8
liamfallon <liam.fallon@est.tech>
2026-09-14 17:18:09 +0100

    Fix coverage into submodules
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- make/testing.mk
+++ make/testing.mk
@@ -25,6 +25,12 @@ test-coverage: ## Generate coverage reports (runs tests with coverage instrument
 	find . -name go.mod -execdir go test -cover -coverprofile=$(TEST_COVERAGE_FILE) ${LDFLAGS} ./... \;
 	find . -name go.mod -execdir go tool cover -html=$(TEST_COVERAGE_FILE) -o $(TEST_COVERAGE_HTML_FILE) \;
 	find . -name go.mod -execdir go tool cover -func=$(TEST_COVERAGE_FILE) -o $(TEST_COVERAGE_FUNC_FILE) \;
+
+	cat api/$(TEST_COVERAGE_FILE) | grep -v "mode: set" >> $(TEST_COVERAGE_FILE)
+	cat mdtogo/$(TEST_COVERAGE_FILE) | grep -v "mode: set" >> $(TEST_COVERAGE_FILE)
+	rm api/$(TEST_COVERAGE_FILE)
+	rm mdtogo/$(TEST_COVERAGE_FILE)
+
 	@echo "Coverage reports generated:"
 	@echo "  - $(TEST_COVERAGE_FILE): Coverage data (for SonarQube)"
 	@echo "  - $(TEST_COVERAGE_HTML_FILE): HTML coverage report"
